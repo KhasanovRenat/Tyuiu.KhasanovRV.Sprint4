343e5caad81ec2f0b40b1ac220d3d84b9620c715
Ренат Хасанов <Tyuiu.KhasanovRV@outlook.com>
2023-11-09 13:58:19 +0500

    Task7

---
+++ Tyuiu.KhasanovRV.Sprint4.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KhasanovRV.Sprint4.Task7.V1.Lib;
+
+namespace Tyuiu.KhasanovRV.Sprint4.Task7.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            string str = "135792468";
+            int wait = 4;
+
+            var res = ds.Calculate(3, 3, str);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KhasanovRV.Sprint4.Task7.V1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KhasanovRV.Sprint4.Task7.V1.Lib;
+
+namespace Tyuiu.KhasanovRV.Sprint4.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Хасанов Р. В. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Хасанов Ренат Владиславович | СМАРТб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр \"135792468\". Преобразуйте ее в      *");
+            Console.WriteLine("* матрицу 3 на 3 и подсчитайте количество четных чисел.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string str = "135792468";
+            Console.WriteLine("Исходная строка: " + str);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var result = ds.Calculate(3, 3, str);
+            Console.WriteLine("Количество четных чисел: " + result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
